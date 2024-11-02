de3d1ca36941c507e39a25fe4bff5563e29ff467
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-11-02 16:51:39 +0100

    updt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,18 @@
 function HomeScreen() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home">
+            <div className="landing">
+                <header>
+                    <h1>Unlock Your potential</h1>
+                    <p>fuel your journey <br /> with  <em>premium nutrition</em> </p>
+                </header>
+                <div className="bg">
+
+                </div>
+                <h3>
+                    At foodspring we're revolutionizing nutrition and fitness. Founded in Berlin in 2013, we've quickly become a leading brand in sports nutrition and functional foods across Europe
+                </h3>
+            </div>
         </div>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ function NavBar() {
                 <button>foodspring</button>
                 <button className="uk">UK</button>
             </div>
-            <div>
+            <div className="about">
                 <button>About Us</button>
                 <button className="special">Shop Now</button>
             </div>
